e69a547bfea3a1854b725c84d923a48ba004839a
Afaque Ahmad <afaqueahmad@Afaques-MacBook-Pro.local>
2018-05-22 22:56:59 +0800

    Starter Files

---
+++ instagramclone/src/test/java/com/cloud/ft05/ca/instagramclone/instagramclone/InstagramcloneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloud.ft05.ca.instagramclone.instagramclone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InstagramcloneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
