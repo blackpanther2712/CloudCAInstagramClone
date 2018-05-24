e93bc82ce37f1f416fbd519d507c17b3ec24237e
Afaque Ahmad <afaqueahmad@Afaques-MacBook-Pro.local>
2018-05-25 02:48:14 +0800

    Updated Project With Dependencies

---
+++ SE_FT05_CLOUD/src/main/java/com/FT05/CloudCA/Repositories/PostRepository.java
@@ -0,0 +1,8 @@
+package com.FT05.CloudCA.Repositories;
+
+import com.FT05.CloudCA.Entity.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
